4fe248921ee8854301e4b8b72948e1fb6c903b17
unknown <cit17fs-23@S312.UC-BCF.EDU.PH>
2022-09-23 12:53:37 +0800

    final

--- ex2.php
+++ ex2.php
@@ -1,6 +1,24 @@
-
 <?php
+//Exercise 5 by Eric John Agapen
 $around = "around";
 echo 'What goes '.$around.', comes '.$around.'.';
 ?>
 
+<?php
+//Excercise 6 by Eric John Agapen
+for ($one = 1; $one < 13; $one++){
+  $square = $one * $one;
+  print($one. "*".$one." = ".$square ."\n" );
+}
+?>
+
+<?php
+//Excercise 7 by Eric John Agapen
+	for ($a=1; $a <= 7; $a++) { 
+		print ("   \n");
+		for ($b=1; $b <= 7; $b++) { 
+		   $p = $b * $a;
+		   print ($p ."   ");
+		   	}
+		}
+?>
